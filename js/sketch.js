8d277d048e802c3335634b68db33ec25e251c7ad
churkle <charfei70@gmail.com>
2015-05-27 13:38:50 -0500

    Finished static grid layout

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,16 @@
 function createGrid(numRows) 
 {
     var rowHeight = $("#sketchpad").height() / numRows;
+    var columnWidth = $("#sketchpad").width() / numRows;
     
     for(var i = 0; i < numRows; i++)
     {
-        $("#sketchpad").append($("<div class=row height=" + rowHeight + "/>"));
+        $("#sketchpad").append($("<div class=row/>").height(rowHeight));
+        
+        for(var j = 0; j < numRows; j++)
+        {
+            $(".row:last-child").append($("<div class=square/>").width(columnWidth));
+        }
     }
 }
 
